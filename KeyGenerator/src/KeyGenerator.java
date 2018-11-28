faeb3f5286322b00e029bc86b615eb91c59a2c58
Jason Thai <jasonnthaii@gmail.com>
2018-11-28 11:01:49 -0800

    Set up neccesary classes

---
+++ KeyGenerator/src/KeyGenerator.java
@@ -0,0 +1,26 @@
+
+public class KeyGenerator {
+	String keystream;
+	
+	private KeyGenerator() {
+		
+	}
+	
+	public KeyGenerator(String key) {
+		
+	}
+	
+	public void generateKeystream(int length) {
+		
+	}
+
+	public String getKeystream() {
+		return keystream;
+	}
+
+	public void setKeystream(String keystream) {
+		this.keystream = keystream;
+	}
+	
+	
+}
---
+++ KeyGenerator/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		String key;
+		int length;
+		String keystream;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter given key for A5/1");
+		key = scanner.nextLine();
+		System.out.println("Please enter wanted size of keystream");
+		length = scanner.nextInt();
+		
+		
+		
+		System.out.println("Here is the keysteam:");
+		
+	}
+}
